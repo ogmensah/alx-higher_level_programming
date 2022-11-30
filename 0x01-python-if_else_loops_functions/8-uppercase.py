90e045b21c61aa2354534c4f6f463536b2ce7ecb
gom6s <gmensah025@gmail.com>
2022-11-30 04:50:58 +0000

    Hello, world

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in str:
+        oc = ord(c)
+        if oc >= 97 and oc <= 122:
+            oc = oc - 32
+        print("{:c}".format(oc), end = '')
+    print()
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    abnum = abs(number)
+    last_num = int(10 * (abnum / 10 - int(abnum / 10)))
+    print(last_num, end = '')
+    return last_num
