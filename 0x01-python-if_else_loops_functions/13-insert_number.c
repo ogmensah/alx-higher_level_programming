1e6633f48ea8b98ae1ca11fe555afbc4da11b21d
gom6s <gmensah025@gmail.com>
2022-11-30 13:54:27 +0000

    Hello, world

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,7 +12,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node, *prev_node, *next_node;
 
-	if (*head == NULL)
+	if (*head == NULL || new_node == NULL)
 	{
 		return (NULL);
 	}
