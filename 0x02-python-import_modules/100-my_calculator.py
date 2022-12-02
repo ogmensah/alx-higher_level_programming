1279e45622a2af09e6fc784277bd1ff2d3c1700e
gom6s <gmensah025@gmail.com>
2022-12-02 04:19:58 +0000

    Import modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if len(sys.argv) != 4:
 op = sys.argv[2]
 n1 = int(sys.argv[1])
 n2 = int(sys.argv[3])
-if op not in ['+','-','*','/']:
+if op not in ['+', '-', '*', '/']:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 
@@ -23,4 +23,3 @@ elif op == '*':
     print(f"{n1} * {n2} = {mul(n1, n2)}")
 else:
     print(f"{n1} / {n2} = {div(n1, n2)}")
-
