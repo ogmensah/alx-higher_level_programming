f94bb2c49783316acf96080d8521c0f8f2b62e3a
gom6s <gmensah025@gmail.com>
2022-12-02 03:04:42 +0000

    Import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print(f"{len(sys.argv) - 1} arguments:")
+        a = 's'
+        if len(sys.argv) == 2: a = ''
+        print(f"{len(sys.argv) - 1} argument{a:s}:")
         for n, arg in enumerate(sys.argv[1:], start=1):
             print(f"{n:d}: {arg}")
