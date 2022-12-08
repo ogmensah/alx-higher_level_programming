26df24edf9dd181096bc889d81034f7e01fda045
gom6s <gmensah025@gmail.com>
2022-12-08 02:59:27 +0000

    python more data structures: set, dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -5,6 +5,6 @@ def simple_delete(a_dictionary, key=""):
     """
     deletes an element based on the key from a dictionary
     """
-    if key is not "" and key in a_dictionary:
+    if isinstance(key, str) and key in a_dictionary:
         a_dictionary.pop(key)
     return (a_dictionary.copy())
