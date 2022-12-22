3b3a54c43bf77d763cd45b44ceddb8656f29a97e
gom6s <gmensah025@gmail.com>
2022-12-22 04:21:27 +0000

    Python exeptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """__init__
         The __init__ method initializes the size value of the square.
-        Attributes:
+        Args:
             size (:obj:`int`, optional): The size of the square.
         Raises:
             TypeError: If `size` type is not `int`.
