dabe5b4d0bb60e464ab0a2e1c904fef0fd27a07f
gom6s <gmensah025@gmail.com>
2023-01-10 17:28:56 +0000

    Python - inheritance project

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Module contains function for printing attributes of class objects"""
-
 def lookup(obj):
     """lookup
     The lookup function searches and displays attributes of objects.
     Attributes:
-        obj: input object.
-    """
+        obj: input object."""
     return dir(obj)
