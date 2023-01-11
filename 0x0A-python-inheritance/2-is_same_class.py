2504adce811fdbfe2c66b13ee5075a86a3936237
gom6s <gmensah025@gmail.com>
2023-01-11 04:03:55 +0000

    Python - inheritance project

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" This module contains function which checks an object is an instance of a class"""
+""" This module contains fxn which checks an obj is instance of a cls"""
 
 
 def is_same_class(obj, a_class):
     """ Checks whether an obj is an instance of a class"""
     if type(obj == a_class:
-        return True
+            return True
     return False
